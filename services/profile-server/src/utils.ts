5f50c2bade4c63c129dfc72238ffea60bef70780
Shedrach Okonofua <shedrachokonofua@gmail.com>
2022-09-27 08:45:28 -0400

    Fix illegal import

--- services/profile-server/src/utils.ts
+++ services/profile-server/src/utils.ts
@@ -1,10 +1,13 @@
-import { buildRymDataClient, buildCatalogClient } from "@lute/shared";
+import {
+  buildRymDataClient,
+  buildCatalogClient,
+  buildRymLookupClient,
+} from "@lute/shared";
 import {
   CATALOG_SERVER_URL,
   RYM_DATA_SERVER_URL,
   RYM_LOOKUP_SERVER_URL,
 } from "./config";
-import { buildRymLookupClient } from "../../../@lute/clients/rym-lookup-client";
 
 export const rymDataClient = buildRymDataClient(RYM_DATA_SERVER_URL);
 
