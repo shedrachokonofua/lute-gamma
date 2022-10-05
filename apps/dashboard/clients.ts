6125c8298549455a8607ac8a9badfdae357c0529
Shedrach Okonofua <shedrachokonofua@gmail.com>
2022-10-05 15:46:48 -0400

    Use different profile url for browser

--- apps/dashboard/clients.ts
+++ apps/dashboard/clients.ts
@@ -1,4 +1,8 @@
 import { buildProfileClient } from "@lute/clients";
 import { PROFILE_SERVER_URL } from "./config";
 
-export const profileClient = buildProfileClient(PROFILE_SERVER_URL);
+const isBrowser = typeof window !== "undefined";
+
+export const profileClient = buildProfileClient(
+  isBrowser ? "http://localhost:3338" : PROFILE_SERVER_URL
+);
