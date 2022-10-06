73b4d8c0c1bd4658d56a1873d58d94bd8b365cb5
Shedrach Okonofua <shedrachokonofua@gmail.com>
2022-10-06 08:24:48 -0400

    Configure dashboard browser profile server url

--- apps/dashboard/clients.ts
+++ apps/dashboard/clients.ts
@@ -1,8 +1,8 @@
 import { buildProfileClient } from "@lute/clients";
-import { PROFILE_SERVER_URL } from "./config";
+import { PROFILE_SERVER_URL, BROWSER_PROFILE_SERVER_URL } from "./config";
 
 const isBrowser = typeof window !== "undefined";
 
 export const profileClient = buildProfileClient(
-  isBrowser ? "http://localhost:3338" : PROFILE_SERVER_URL
+  isBrowser ? BROWSER_PROFILE_SERVER_URL : PROFILE_SERVER_URL
 );
--- apps/dashboard/config.ts
+++ apps/dashboard/config.ts
@@ -4,3 +4,8 @@ export const PROFILE_SERVER_URL = env
   .get("PROFILE_SERVER_URL")
   .default("http://profile-server")
   .asString();
+
+export const BROWSER_PROFILE_SERVER_URL = env
+  .get("BROWSER_PROFILE_SERVER_URL")
+  .default("http://localhost:3338")
+  .asString();
