1ed618107d9d5ce40c52f7ded992399212824a35
Shedrach Okonofua <shedrachokonofua@gmail.com>
2022-10-06 09:07:10 -0400

    Fix dashboard env

--- apps/dashboard/config.ts
+++ apps/dashboard/config.ts
@@ -5,7 +5,6 @@ export const PROFILE_SERVER_URL = env
   .default("http://profile-server")
   .asString();
 
-export const BROWSER_PROFILE_SERVER_URL = env
-  .get("NEXT_PUBLIC_PROFILE_SERVER_URL")
-  .default("http://localhost:3338")
-  .asString();
+export const BROWSER_PROFILE_SERVER_URL =
+  (process.env.NEXT_PUBLIC_PROFILE_SERVER_URL as string) ||
+  "http://localhost:3338";
