28338596cea60d6b302b08ad0a7597355907e01c
Shedrach Okonofua <shedrachokonofua@gmail.com>
2022-11-04 21:21:26 -0400

    Use worker context in cron jobs

--- apps/lute/src/jobs/reset-crawler-quota.ts
+++ apps/lute/src/jobs/reset-crawler-quota.ts
@@ -1,14 +1,15 @@
 import { parentPort } from "node:worker_threads";
 import process from "node:process";
 import { runWithTraceId } from "../lib";
-import { buildContext } from "../context";
+import { buildWorkerContext } from "../context";
 import { logger } from "../logger";
 
 (async () => {
   await runWithTraceId(async () => {
-    const context = await buildContext();
+    const context = await buildWorkerContext();
     logger.info("Reset crawler quota job started");
     await context.crawlerInteractor.resetQuota();
+    await context.terminate();
     logger.info("Reset crawler quota job finished");
   });
   parentPort?.postMessage("done");
--- apps/lute/src/jobs/seed-default-profile.ts
+++ apps/lute/src/jobs/seed-default-profile.ts
@@ -1,15 +1,16 @@
 import { parentPort } from "node:worker_threads";
 import process from "node:process";
-import { buildContext } from "../context";
+import { buildWorkerContext } from "../context";
 import { runWithTraceId } from "../lib";
 import { logger } from "../logger";
 import { seedDefaultProfile } from "../modules/profile/seeders";
 
 (async () => {
   await runWithTraceId(async () => {
-    const context = await buildContext();
+    const context = await buildWorkerContext();
     logger.info("Seed default profile job started");
     await seedDefaultProfile(context);
+    await context.terminate();
     logger.info("Seed default profile job finished");
   });
   parentPort?.postMessage("done");
