ed66fdb3eafb061c1a62cac5ad33412497fd0d1d
Shedrach Okonofua <shedrachokonofua@gmail.com>
2023-04-30 20:44:31 -0400

    Move grafana stack setup off cloud onto  docker compose in dev

--- apps/lute/src/logger.ts
+++ apps/lute/src/logger.ts
@@ -1,27 +1,22 @@
 import pino from "pino";
 import * as rTracer from "cls-rtracer";
-import { config } from "./config";
 
-export const logger = pino(
-  {
-    mixin() {
-      return {
-        rTraceId: rTracer.id(),
-      };
-    },
-    level: "trace",
+export const logger = pino({
+  mixin() {
+    return {
+      rTraceId: rTracer.id(),
+    };
   },
-  pino.transport({
-    target: "pino-loki",
-    options: {
-      host: config.loki.host,
-      basicAuth: {
-        username: config.loki.username,
-        password: config.loki.password,
-      },
-      labels: {
-        env: config.env,
-      },
-    },
-  })
-);
+  level: "trace",
+});
+
+// Ensure unhandled errors are logged
+process.on("unhandledRejection", (reason: any, promise) => {
+  logger.error({ promise }, "Unhandled Rejection at:", reason.stack || reason);
+});
+
+// Ensure uncaught exceptions are logged
+process.on("uncaughtException", (err) => {
+  logger.fatal(err, "Uncaught Exception thrown");
+  process.exit(1);
+});
